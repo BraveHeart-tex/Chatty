0ee4c3505ea256de32dfef62c4946f329de0f0ef
Bora <bora@Bora-MacBook-Air.local>
2023-04-22 21:46:15 +0300

    backend: implement user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userModel = mongoose.Schema(
+  {
+    name: { type: String, trim: true },
+    email: { type: String, trim: true },
+    password: { type: String, trim: true },
+    picture: {
+      type: String,
+      required: true,
+      default:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJdGouD75_lVEncH-Hu1naif3TDh6VKv3iwZqz2t6mOA7YkG1j&s',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userModel);
+
+module.exports = User;
