6c60b8d60408d1b2e04e754913c726ea2972b1a4
Bora <bora@Bora-MacBook-Air.local>
2023-04-23 11:22:56 +0300

    backend: implement connection to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const { chats } = require('./dummy-data/data');
+const express = require("express");
+const { chats } = require("./dummy-data/data");
+const { connectToDB } = require("./config/db");
+
+connectToDB().then(() => {
+  console.log("Connected to MongoDB");
+});
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.get('/api/chat', (req, res) => {
+app.get("/api/chat", (req, res) => {
   res.send(chats);
 });
 
-app.get('/api/chat/:id', (req, res) => {
+app.get("/api/chat/:id", (req, res) => {
   const { id } = req.params;
   const chat = chats.find((chat) => chat._id === id);
   res.send(chat);
