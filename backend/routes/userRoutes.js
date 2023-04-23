2d66bd15649113543703631733880b93f61923eb
Bora <bora@Bora-MacBook-Air.local>
2023-04-23 14:28:54 +0300

    backend: implement getAllUsers controller and route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controllers/userControllers");
+const {
+  registerUser,
+  authUser,
+  getAllUsers,
+} = require("../controllers/userControllers");
 
 const router = express.Router();
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(getAllUsers);
 router.route("/login").post(authUser);
 
 module.exports = router;
