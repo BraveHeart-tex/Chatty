f1b71335615e2f3ec33c4e4fe59261fdc118ab0b
Bora <bora@Bora-MacBook-Air.local>
2023-04-23 15:18:41 +0300

    backend: implement accessing chat functionality

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { protect } = require("../middlewares/authMiddleware");
+const { accessChat } = require("../controllers/chatController");
+
 const router = express.Router();
 
-// router.route("/").post(protect, accessChat);
+router.route("/").post(protect, accessChat);
 // router.route("/").get(protect, fetchChats);
 // router.route("/group").post(protect, createGroupChat);
 // router.route("/group/rename").put(protect, renameGroupChat);
