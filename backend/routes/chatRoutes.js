bb091668752db6502c358cd0a2dee985cdb092d1
Bora <bora@Bora-MacBook-Air.local>
2023-04-23 16:02:34 +0300

    backend: implement create group chat functionality

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const { protect } = require("../middlewares/authMiddleware");
-const { accessChat } = require("../controllers/chatController");
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  renameGroupChat,
+} = require("../controllers/chatController");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
-// router.route("/").get(protect, fetchChats);
-// router.route("/group").post(protect, createGroupChat);
-// router.route("/group/rename").put(protect, renameGroupChat);
+router.route("/").get(protect, fetchChats);
+router.route("/group").post(protect, createGroupChat);
+router.route("/group/rename").put(protect, renameGroupChat);
 // router.route("/group/remove").put(protect, removeFromGroup);
 // router.route("/group/add").put(protect, addToGroup);
 
