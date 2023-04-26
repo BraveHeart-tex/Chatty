fe27520dc08f6dd1a3d024f5aa515f52f8741e5a
Bora <bora@Bora-MacBook-Air.local>
2023-04-26 16:55:25 +0300

    backend: implement message routes/controllers and unit tests

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,51 @@
+const expressAsyncHandler = require("express-async-handler");
+const Message = require("../models/messageModel");
+const User = require("../models/userModel");
+const Chat = require("../models/chatModel");
+const MessageService = require("../services/messageService");
+
+const sendMessage = async (req, res) => {
+  const { content, chatId } = req.body;
+
+  if (!content || !chatId) {
+    res.status(400).send("Content and chatId are required");
+    throw new Error("Content and chatId are required");
+  }
+
+  try {
+    const message = await MessageService.createMessage(
+      req.user._id,
+      content,
+      chatId
+    );
+    res.status(201);
+    res.json(message);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+};
+
+const getAllMessages = async (req, res) => {
+  const { chatId } = req.params;
+
+  if (!chatId) {
+    console.log("ChatId is required");
+    res.status(400).send("ChatId is required");
+    throw new Error("ChatId is required");
+  }
+
+  try {
+    const messages = await MessageService.getAllMessages(chatId);
+    res.status(200);
+    res.json(messages);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+};
+
+module.exports = {
+  sendMessage,
+  getAllMessages,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,25 @@
-require("dotenv").config();
+require('dotenv').config();
+const express = require('express');
 
-const express = require("express");
-const { connectToDB } = require("./config/db");
-const userRoutes = require("./routes/userRoutes");
-const chatRoutes = require("./routes/chatRoutes");
-const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const { connectToDB } = require('./config/db');
+
+const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
+const messageRoutes = require('./routes/messageRoutes');
+
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 
 connectToDB().then(() => {
-  console.log("Connected to MongoDB");
+  console.log('Connected to MongoDB');
 });
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/user", userRoutes);
-app.use("/api/chat", chatRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/chat', chatRoutes);
+app.use('/api/message', messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
