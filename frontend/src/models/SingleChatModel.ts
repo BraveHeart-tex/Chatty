d6311ace307878c634f72918f1f922f429bfd13e
Bora <bora@Bora-MacBook-Air.local>
2023-04-27 17:45:02 +0300

    implement realtime notification and prepare for deployment

---
+++ frontend/src/models/SingleChatModel.ts
@@ -0,0 +1,19 @@
+export type SingleChatModel = ChatsChild;
+export interface ChatsChildUsers {
+  _id: string;
+  name: string;
+  email: string;
+  picture: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
+export interface ChatsChild {
+  _id: string;
+  chatName: string;
+  isGroupChat: boolean;
+  users: ChatsChildUsers[];
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
