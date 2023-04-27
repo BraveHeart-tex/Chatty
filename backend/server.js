7db47a6abb8cdbe7d5f3cbfd1831d418f886f2f0
Bora <bora@Bora-MacBook-Air.local>
2023-04-27 13:03:02 +0300

    backend: implement initial socket.io structure

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,42 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 
-const { connectToDB } = require('./config/db');
+const { connectToDB } = require("./config/db");
 
-const userRoutes = require('./routes/userRoutes');
-const chatRoutes = require('./routes/chatRoutes');
-const messageRoutes = require('./routes/messageRoutes');
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 
-const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 connectToDB().then(() => {
-  console.log('Connected to MongoDB');
+  console.log("Connected to MongoDB");
 });
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/user', userRoutes);
-app.use('/api/chat', chatRoutes);
-app.use('/api/message', messageRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
 });
