485480f3f456ad423445c03c677d5ea79d68f39f
Bora <bora@Bora-MacBook-Air.local>
2023-05-01 11:38:59 +0300

    implement react query for fetching chats

--- frontend/src/components/MyChats.tsx
+++ frontend/src/components/MyChats.tsx
@@ -1,76 +1,65 @@
-import { useChatState } from "../context/ChatProvider.tsx";
-import { useEffect, useState } from "react";
-import { Box, Button, Stack, Text, useToast } from "@chakra-ui/react";
-import axios from "axios";
-import ChatLoading from "./ChatLoading.tsx";
-import { getSender } from "../config/ChatConfig.ts";
-import { User } from "../models/User.ts";
-import GroupChatModal from "./mixed/GroupChatModal.tsx";
-import { PlusSquareIcon } from "@chakra-ui/icons";
+import { useChatState } from '../context/ChatProvider.tsx';
+import { useEffect } from 'react';
+import { Box, Button, Stack, Text, useToast } from '@chakra-ui/react';
+import axios from 'axios';
+import ChatLoading from './ChatLoading.tsx';
+import { getSender } from '../config/ChatConfig.ts';
+import GroupChatModal from './mixed/GroupChatModal.tsx';
+import { PlusSquareIcon } from '@chakra-ui/icons';
+import { useQuery } from '@tanstack/react-query';
+import { ChatsChild } from '../models/Chats.ts';
 
 interface IMyChatsProps {
   fetchAgain: boolean;
 }
 const MyChats = ({ fetchAgain }: IMyChatsProps) => {
-  const [loggedUser, setLoggedUser] = useState<User>();
-  const { selectedChat, setSelectedChat, user, chats, setChats } =
-    useChatState();
+  const { selectedChat, setSelectedChat, user, setChats } = useChatState();
   const toast = useToast();
+  const loggedUser = JSON.parse(localStorage.getItem('userInfo') as string);
 
-  const fetchChats = async () => {
-    try {
-      const config = {
-        headers: {
-          Authorization: `Bearer ${user?.token}`,
-        },
-      };
-
-      const { data } = await axios.get("/api/chat", config);
-      setChats(data);
-    } catch (error) {
-      toast({
-        title: "Error occurred!",
-        description: error.message,
-        status: "error",
-        duration: 4000,
-        isClosable: true,
-        position: "bottom-left",
-      });
-    }
-  };
+  const { data: chats, isLoading } = useQuery(['myChats'], async () => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user?.token}`,
+      },
+    };
+    const { data } = await axios.get('/api/chat', config);
+    return data;
+  });
 
   useEffect(() => {
-    setLoggedUser(JSON.parse(localStorage.getItem("userInfo") as string));
-    fetchChats();
-  }, [fetchAgain]);
+    if (fetchAgain) {
+      setChats(chats);
+    }
+  }, [chats, fetchAgain, setChats]);
 
   return (
     <Box
-      display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
-      flexDirection="column"
-      alignItems="center"
+      display={{ base: selectedChat ? 'none' : 'flex', md: 'flex' }}
+      flexDirection='column'
+      alignItems='center'
       p={3}
-      bg="#edf2f4"
-      w={{ base: "100%", md: "31%" }}
-      borderRadius="lg"
-      borderWidth="1px"
+      bg='#edf2f4'
+      w={{ base: '100%', md: '31%' }}
+      borderRadius='lg'
+      borderWidth='1px'
     >
       <Box
         pb={3}
         px={3}
-        fontSize={{ base: "28px", md: "30px" }}
-        fontFamily="Work sans"
-        display="flex"
-        w="100%"
-        justifyContent="space-between"
-        alignItems="center"
-        color="blue.400"
+        fontSize={{ base: '28px', md: '30px' }}
+        fontFamily='Work sans'
+        display='flex'
+        w='100%'
+        justifyContent='space-between'
+        alignItems='center'
+        color='blue.400'
       >
         My Chats
         <GroupChatModal>
           <Button
-            display="flex"
-            fontSize={{ base: "17px", md: "15px", lg: "17px" }}
+            display='flex'
+            fontSize={{ base: '17px', md: '15px', lg: '17px' }}
             rightIcon={<PlusSquareIcon />}
           >
             New Group Chat
@@ -78,26 +67,26 @@ const MyChats = ({ fetchAgain }: IMyChatsProps) => {
         </GroupChatModal>
       </Box>
       <Box
-        display="flex"
-        flexDirection="column"
+        display='flex'
+        flexDirection='column'
         p={3}
-        bg="#F8F8F8"
-        w="100%"
-        h="100%"
-        borderRadius="lg"
-        overflowY="hidden"
+        bg='#F8F8F8'
+        w='100%'
+        h='100%'
+        borderRadius='lg'
+        overflowY='hidden'
       >
         {chats ? (
-          <Stack overflowY="scroll">
-            {chats.map((chat) => (
+          <Stack overflowY='scroll'>
+            {chats.map((chat: ChatsChild) => (
               <Box
                 onClick={() => setSelectedChat(chat)}
-                cursor="pointer"
-                bg={selectedChat === chat ? "blue.300" : "blue.200"}
-                color={selectedChat === chat ? "white" : "facebook.500"}
+                cursor='pointer'
+                bg={selectedChat === chat ? 'blue.300' : 'blue.200'}
+                color={selectedChat === chat ? 'white' : 'facebook.500'}
                 px={3}
                 py={2}
-                borderRadius="lg"
+                borderRadius='lg'
                 key={chat._id}
               >
                 <Text>
--- frontend/src/components/SingleChat.tsx
+++ frontend/src/components/SingleChat.tsx
@@ -1,4 +1,4 @@
-import { useChatState } from "../context/ChatProvider.tsx";
+import { useChatState } from '../context/ChatProvider.tsx';
 import {
   Box,
   FormControl,
@@ -7,27 +7,27 @@ import {
   Spinner,
   Text,
   useToast,
-} from "@chakra-ui/react";
-import { ArrowBackIcon } from "@chakra-ui/icons";
-import { getSender, getSenderFull } from "../config/ChatConfig.ts";
-import ProfileModal from "./mixed/ProfileModal.tsx";
-import capitalizeWords from "../utils/capitalizeWords.ts";
-import UpdateGroupChatModal from "./mixed/UpdateGroupChatModal";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Message } from "../models/Message.ts";
-import "./styles.css";
-import ScrollableChat from "./ScrollableChat.tsx";
-import { io, Socket } from "socket.io-client";
-import Lottie from "react-lottie";
-import animationData from "../animations/typing.json";
+} from '@chakra-ui/react';
+import { ArrowBackIcon } from '@chakra-ui/icons';
+import { getSender, getSenderFull } from '../config/ChatConfig.ts';
+import ProfileModal from './mixed/ProfileModal.tsx';
+import capitalizeWords from '../utils/capitalizeWords.ts';
+import UpdateGroupChatModal from './mixed/UpdateGroupChatModal';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Message } from '../models/Message.ts';
+import './styles.css';
+import ScrollableChat from './ScrollableChat.tsx';
+import { io, Socket } from 'socket.io-client';
+import Lottie from 'react-lottie';
+import animationData from '../animations/typing.json';
 
 interface ISingleChatProps {
   fetchAgain: boolean;
   setFetchAgain: (fetchAgain: boolean) => void;
 }
 
-const ENDPOINT = "http://localhost:4000";
+const ENDPOINT = 'http://localhost:4000';
 let socket: Socket, selectedChatCompare: any;
 const SingleChat = ({ fetchAgain, setFetchAgain }: ISingleChatProps) => {
   const [messages, setMessages] = useState<Message[]>([]);
@@ -46,7 +46,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }: ISingleChatProps) => {
     autoplay: true,
     animationData,
     rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
+      preserveAspectRatio: 'xMidYMid slice',
     },
   };
 
@@ -70,25 +70,25 @@ const SingleChat = ({ fetchAgain, setFetchAgain }: ISingleChatProps) => {
       setMessages(data);
       setLoading(false);
 
-      socket.emit("join chat", selectedChat._id);
+      socket.emit('join chat', selectedChat._id);
     } catch (error) {
       toast({
-        title: "Error occurred!",
-        description: "Failed to fetch conversation",
-        status: "error",
+        title: 'Error occurred!',
+        description: 'Failed to fetch conversation',
+        status: 'error',
         duration: 4000,
         isClosable: true,
-        position: "top-right",
+        position: 'top-right',
       });
     }
   };
 
   useEffect(() => {
     socket = io(ENDPOINT);
-    socket.emit("setup", user);
-    socket.on("connected", () => setSocketConnected(true));
-    socket.on("typing", () => setIsTyping(true));
-    socket.on("stop typing", () => setIsTyping(false));
+    socket.emit('setup', user);
+    socket.on('connected', () => setSocketConnected(true));
+    socket.on('typing', () => setIsTyping(true));
+    socket.on('stop typing', () => setIsTyping(false));
   }, []);
 
   useEffect(() => {
@@ -98,7 +98,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }: ISingleChatProps) => {
   }, [selectedChat]);
 
   useEffect(() => {
-    socket.on("message received", (newMessageReceived) => {
+    socket.on('message received', (newMessageReceived) => {
       if (
         !selectedChatCompare ||
         selectedChatCompare._id !== newMessageReceived.chat._id
@@ -121,7 +121,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }: ISingleChatProps) => {
 
     if (!typing) {
       setTyping(true);
-      socket.emit("typing", selectedChat?._id);
+      socket.emit('typing', selectedChat?._id);
     }
 
     let lastTypingTime = new Date().getTime();
@@ -132,41 +132,41 @@ const SingleChat = ({ fetchAgain, setFetchAgain }: ISingleChatProps) => {
       let timeDifference = now - lastTypingTime;
 
       if (timeDifference >= timerDuration && typing) {
-        socket.emit("stop typing", selectedChat?._id);
+        socket.emit('stop typing', selectedChat?._id);
         setTyping(false);
       }
     }, timerDuration);
   };
 
   const sendMessage = async (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter" && newMessage) {
-      socket.emit("stop typing", selectedChat?._id);
+    if (e.key === 'Enter' && newMessage) {
+      socket.emit('stop typing', selectedChat?._id);
       try {
         const config = {
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
             Authorization: `Bearer ${user?.token}`,
           },
         };
 
-        setNewMessage("");
+        setNewMessage('');
 
         const { data } = await axios.post(
-          "api/message",
+          'api/message',
           { content: newMessage, chatId: selectedChat?._id },
           config
         );
 
-        socket.emit("new message", data);
+        socket.emit('new message', data);
         setMessages((prevMessages) => [...prevMessages, data]);
       } catch (error) {
         toast({
-          title: "Error occurred!",
-          description: "Failed to send your message",
-          status: "error",
+          title: 'Error occurred!',
+          description: 'Failed to send your message',
+          status: 'error',
           duration: 4000,
           isClosable: true,
-          position: "top-right",
+          position: 'top-right',
         });
       }
     }
@@ -177,18 +177,18 @@ const SingleChat = ({ fetchAgain, setFetchAgain }: ISingleChatProps) => {
       {selectedChat ? (
         <>
           <Text
-            fontSize={{ base: "28px", md: "30px" }}
+            fontSize={{ base: '28px', md: '30px' }}
             pb={3}
             px={2}
-            w="100%"
-            fontFamily="Work sans"
-            display="flex"
-            justifyContent={{ base: "space-between" }}
-            alignItems="center"
+            w='100%'
+            fontFamily='Work sans'
+            display='flex'
+            justifyContent={{ base: 'space-between' }}
+            alignItems='center'
           >
             <IconButton
               aria-label={`Back to chats`}
-              display={{ base: "flex", md: "none" }}
+              display={{ base: 'flex', md: 'none' }}
               icon={<ArrowBackIcon />}
               onClick={() => setSelectedChat(undefined)}
             />
@@ -196,7 +196,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }: ISingleChatProps) => {
             {!selectedChat.isGroupChat ? (
               <>
                 {
-                  <Text color="facebook.400">
+                  <Text color='facebook.400'>
                     {getSender(user, selectedChat.users)}
                   </Text>
                 }
@@ -205,7 +205,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }: ISingleChatProps) => {
             ) : (
               <>
                 {
-                  <Text color="facebook.400">
+                  <Text color='facebook.400'>
                     {capitalizeWords(selectedChat.chatName)}
                   </Text>
                 }
@@ -218,27 +218,27 @@ const SingleChat = ({ fetchAgain, setFetchAgain }: ISingleChatProps) => {
             )}
           </Text>
           <Box
-            display="flex"
-            flexDirection="column"
-            justifyContent="flex-end"
+            display='flex'
+            flexDirection='column'
+            justifyContent='flex-end'
             p={3}
-            bg="whiteAlpha.500"
-            w="100%"
-            h="100%"
-            borderRadius="lg"
-            overflowY="hidden"
+            bg='whiteAlpha.500'
+            w='100%'
+            h='100%'
+            borderRadius='lg'
+            overflowY='hidden'
           >
             {loading ? (
               <Spinner
-                size="xl"
+                size='xl'
                 w={20}
                 h={20}
-                alignSelf="center"
-                margin="auto"
-                color="blue.400"
+                alignSelf='center'
+                margin='auto'
+                color='blue.400'
               />
             ) : (
-              <div className="messages">
+              <div className='messages'>
                 <ScrollableChat messages={messages}></ScrollableChat>
               </div>
             )}
@@ -255,9 +255,9 @@ const SingleChat = ({ fetchAgain, setFetchAgain }: ISingleChatProps) => {
                 <></>
               )}
               <Input
-                variant="filled"
-                bg="white"
-                placeholder="Enter a message..."
+                variant='filled'
+                bg='white'
+                placeholder='Enter a message...'
                 onChange={handleType}
                 value={newMessage}
               />
@@ -266,12 +266,12 @@ const SingleChat = ({ fetchAgain, setFetchAgain }: ISingleChatProps) => {
         </>
       ) : (
         <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          h="100%"
+          display='flex'
+          alignItems='center'
+          justifyContent='center'
+          h='100%'
         >
-          <Text fontSize="3xl" pb={3} fontFamily="Work sans" color="blue.400">
+          <Text fontSize='3xl' pb={3} fontFamily='Work sans' color='blue.400'>
             Select a user from "My Chats" to start chatting
           </Text>
         </Box>
