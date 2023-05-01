a91be2a0db5893a3b57d5bfea38c87c97b3a6858
Bora <bora@Bora-MacBook-Air.local>
2023-05-01 11:02:47 +0300

    implement 404 page

---
+++ frontend/src/pages/NotFoundPage.tsx
@@ -0,0 +1,63 @@
+import {
+  Box,
+  Heading,
+  Container,
+  Text,
+  Button,
+  Stack,
+  Icon,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+
+const NotFoundPage = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Container
+        maxW={'100%'}
+        bg='blue.200'
+        display={'flex'}
+        flexDirection='column'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <Stack as={Box} spacing={{ base: 8, md: 14 }} py={{ base: 20, md: 36 }}>
+          <Heading
+            fontWeight={700}
+            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}
+            color={'white'}
+          >
+            Sorry, the page your looking for
+            <Text as={'u'}> doesn't exist</Text> 😔
+          </Heading>
+          <Text fontSize='26px' color='white'>
+            You probably tried to navigate to a page that doesn't exist yet.
+          </Text>
+          <Stack
+            direction={'column'}
+            spacing={3}
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}
+          >
+            <Button
+              bg={'blue.400'}
+              color={'white'}
+              rounded={'full'}
+              px={6}
+              _hover={{
+                bg: 'blue.500',
+              }}
+              onClick={() => navigate('/chats')}
+            >
+              Go to home page
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+    </>
+  );
+};
+export default NotFoundPage;
