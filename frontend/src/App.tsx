838a7bf15eaad29319577bc4e019cb2c722cec04
Bora <bora@Bora-MacBook-Air.local>
2023-05-01 09:51:19 +0300

    implement 404 page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import ChatsPage from "./pages/ChatsPage.tsx";
+import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ChatsPage from './pages/ChatsPage.tsx';
+import NotFoundPage from './pages/NotFoundPage.tsx';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Routes>
-        <Route element={<HomePage />} path="/login" />
-        <Route element={<ChatsPage />} path="/chats" />
+        <Route element={<NotFoundPage />} path='*'></Route>
+        <Route element={<Navigate replace to='/chats' />} path='/'></Route>
+        <Route element={<HomePage />} path='/login' />
+        <Route element={<ChatsPage />} path='/chats' />
       </Routes>
     </div>
   );
